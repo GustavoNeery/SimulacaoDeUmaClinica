148ad6204e27d6d8ffe2c53f22f9e775a5a1af2e
Luiz Gustavo <gustavoalvesneryy@hotmail.com>
2021-08-05 11:58:04 -0300

    Correção da linha adicionada

--- Sistema_De_Uma_Clinica_UEPB.c
+++ Sistema_De_Uma_Clinica_UEPB.c
@@ -310,7 +310,6 @@ void listar(){
             printf("\t\t\t\tCódigo: %s\n",ctt.codigo);
             printf("\t\t\t\tCPF: %s\n",ctt.cpf);
             printf("\t\t\t\t-----------------------------------------------\n\n");
-	    printf("");
         }
 
     }
